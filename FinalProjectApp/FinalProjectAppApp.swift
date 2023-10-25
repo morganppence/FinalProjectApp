672b08dfe1136962afa987c50e8c54e3784e0acb
morganppence <morgan.pence@colorado.edu>
2023-10-25 13:09:53 -0600

    Initial Commit

---
+++ FinalProjectApp/FinalProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectAppApp.swift
+//  FinalProjectApp
+//
+//  Created by Morgan Pence on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
